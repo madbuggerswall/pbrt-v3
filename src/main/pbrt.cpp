a735974f8cd6934b45cfa90f269f1643ba54c222
Ege Furkan Bilgin <furkanegebilgin@gmail.com>
2020-12-15 20:45:52 +0300

    Initial commit. Updated .clang-format

--- src/main/pbrt.cpp
+++ src/main/pbrt.cpp
@@ -30,21 +30,21 @@
 
  */
 
-
 // main/pbrt.cpp*
 #include "pbrt.h"
-#include "api.h"
-#include "parser.h"
-#include "parallel.h"
+
 #include <glog/logging.h>
+
+#include "api.h"
+#include "parallel.h"
+#include "parser.h"
 
 using namespace pbrt;
 
 static void usage(const char *msg = nullptr) {
-    if (msg)
-        fprintf(stderr, "pbrt: %s\n\n", msg);
+  if (msg) fprintf(stderr, "pbrt: %s\n\n", msg);
 
-    fprintf(stderr, R"(usage: pbrt [<options>] <filename.pbrt...>
+  fprintf(stderr, R"(usage: pbrt [<options>] <filename.pbrt...>
 Rendering options:
   --cropwindow <x0,x1,y0,y1> Specify an image crop window.
   --help               Print this help text.
@@ -69,105 +69,95 @@ Reformatting options:
                        standard output and convert all triangle meshes to
                        PLY files. Does not render an image.
 )");
-    exit(msg ? 1 : 0);
+  exit(msg ? 1 : 0);
 }
 
 // main program
 int main(int argc, char *argv[]) {
-    google::InitGoogleLogging(argv[0]);
-    FLAGS_stderrthreshold = 1; // Warning and above.
+  google::InitGoogleLogging(argv[0]);
+  FLAGS_stderrthreshold = 1;  // Warning and above.
 
-    Options options;
-    std::vector<std::string> filenames;
-    // Process command-line arguments
-    for (int i = 1; i < argc; ++i) {
-        if (!strcmp(argv[i], "--nthreads") || !strcmp(argv[i], "-nthreads")) {
-            if (i + 1 == argc)
-                usage("missing value after --nthreads argument");
-            options.nThreads = atoi(argv[++i]);
-        } else if (!strncmp(argv[i], "--nthreads=", 11)) {
-            options.nThreads = atoi(&argv[i][11]);
-        } else if (!strcmp(argv[i], "--outfile") || !strcmp(argv[i], "-outfile")) {
-            if (i + 1 == argc)
-                usage("missing value after --outfile argument");
-            options.imageFile = argv[++i];
-        } else if (!strcmp(argv[i], "--cropwindow") || !strcmp(argv[i], "-cropwindow")) {
-            if (i + 4 >= argc)
-                usage("missing value after --cropwindow argument");
-            options.cropWindow[0][0] = atof(argv[++i]);
-            options.cropWindow[0][1] = atof(argv[++i]);
-            options.cropWindow[1][0] = atof(argv[++i]);
-            options.cropWindow[1][1] = atof(argv[++i]);
-        } else if (!strncmp(argv[i], "--outfile=", 10)) {
-            options.imageFile = &argv[i][10];
-        } else if (!strcmp(argv[i], "--logdir") || !strcmp(argv[i], "-logdir")) {
-            if (i + 1 == argc)
-                usage("missing value after --logdir argument");
-            FLAGS_log_dir = argv[++i];
-        } else if (!strncmp(argv[i], "--logdir=", 9)) {
-            FLAGS_log_dir = &argv[i][9];
-        } else if (!strcmp(argv[i], "--minloglevel") ||
-                   !strcmp(argv[i], "-minloglevel")) {
-            if (i + 1 == argc)
-                usage("missing value after --minloglevel argument");
-            FLAGS_minloglevel = atoi(argv[++i]);
-        } else if (!strncmp(argv[i], "--minloglevel=", 14)) {
-            FLAGS_minloglevel = atoi(&argv[i][14]);
-        } else if (!strcmp(argv[i], "--quick") || !strcmp(argv[i], "-quick")) {
-            options.quickRender = true;
-        } else if (!strcmp(argv[i], "--quiet") || !strcmp(argv[i], "-quiet")) {
-            options.quiet = true;
-        } else if (!strcmp(argv[i], "--cat") || !strcmp(argv[i], "-cat")) {
-            options.cat = true;
-        } else if (!strcmp(argv[i], "--toply") || !strcmp(argv[i], "-toply")) {
-            options.toPly = true;
-        } else if (!strcmp(argv[i], "--v") || !strcmp(argv[i], "-v")) {
-            if (i + 1 == argc)
-                usage("missing value after --v argument");
-            FLAGS_v = atoi(argv[++i]);
-        } else if (!strncmp(argv[i], "--v=", 4)) {
-          FLAGS_v = atoi(argv[i] + 4);
-        }
-        else if (!strcmp(argv[i], "--logtostderr")) {
-          FLAGS_logtostderr = true;
-        } else if (!strcmp(argv[i], "--help") || !strcmp(argv[i], "-help") ||
-                   !strcmp(argv[i], "-h")) {
-            usage();
-            return 0;
-        } else
-            filenames.push_back(argv[i]);
-    }
+  Options options;
+  std::vector<std::string> filenames;
+  // Process command-line arguments
+  for (int i = 1; i < argc; ++i) {
+    if (!strcmp(argv[i], "--nthreads") || !strcmp(argv[i], "-nthreads")) {
+      if (i + 1 == argc) usage("missing value after --nthreads argument");
+      options.nThreads = atoi(argv[++i]);
+    } else if (!strncmp(argv[i], "--nthreads=", 11)) {
+      options.nThreads = atoi(&argv[i][11]);
+    } else if (!strcmp(argv[i], "--outfile") || !strcmp(argv[i], "-outfile")) {
+      if (i + 1 == argc) usage("missing value after --outfile argument");
+      options.imageFile = argv[++i];
+    } else if (!strcmp(argv[i], "--cropwindow") || !strcmp(argv[i], "-cropwindow")) {
+      if (i + 4 >= argc) usage("missing value after --cropwindow argument");
+      options.cropWindow[0][0] = atof(argv[++i]);
+      options.cropWindow[0][1] = atof(argv[++i]);
+      options.cropWindow[1][0] = atof(argv[++i]);
+      options.cropWindow[1][1] = atof(argv[++i]);
+    } else if (!strncmp(argv[i], "--outfile=", 10)) {
+      options.imageFile = &argv[i][10];
+    } else if (!strcmp(argv[i], "--logdir") || !strcmp(argv[i], "-logdir")) {
+      if (i + 1 == argc) usage("missing value after --logdir argument");
+      FLAGS_log_dir = argv[++i];
+    } else if (!strncmp(argv[i], "--logdir=", 9)) {
+      FLAGS_log_dir = &argv[i][9];
+    } else if (!strcmp(argv[i], "--minloglevel") || !strcmp(argv[i], "-minloglevel")) {
+      if (i + 1 == argc) usage("missing value after --minloglevel argument");
+      FLAGS_minloglevel = atoi(argv[++i]);
+    } else if (!strncmp(argv[i], "--minloglevel=", 14)) {
+      FLAGS_minloglevel = atoi(&argv[i][14]);
+    } else if (!strcmp(argv[i], "--quick") || !strcmp(argv[i], "-quick")) {
+      options.quickRender = true;
+    } else if (!strcmp(argv[i], "--quiet") || !strcmp(argv[i], "-quiet")) {
+      options.quiet = true;
+    } else if (!strcmp(argv[i], "--cat") || !strcmp(argv[i], "-cat")) {
+      options.cat = true;
+    } else if (!strcmp(argv[i], "--toply") || !strcmp(argv[i], "-toply")) {
+      options.toPly = true;
+    } else if (!strcmp(argv[i], "--v") || !strcmp(argv[i], "-v")) {
+      if (i + 1 == argc) usage("missing value after --v argument");
+      FLAGS_v = atoi(argv[++i]);
+    } else if (!strncmp(argv[i], "--v=", 4)) {
+      FLAGS_v = atoi(argv[i] + 4);
+    } else if (!strcmp(argv[i], "--logtostderr")) {
+      FLAGS_logtostderr = true;
+    } else if (!strcmp(argv[i], "--help") || !strcmp(argv[i], "-help") || !strcmp(argv[i], "-h")) {
+      usage();
+      return 0;
+    } else
+      filenames.push_back(argv[i]);
+  }
 
-    // Print welcome banner
-    if (!options.quiet && !options.cat && !options.toPly) {
-        if (sizeof(void *) == 4)
-            printf("*** WARNING: This is a 32-bit build of pbrt. It will crash "
-                   "if used to render highly complex scenes. ***\n");
-        printf("pbrt version 3 (built %s at %s) [Detected %d cores]\n",
-               __DATE__, __TIME__, NumSystemCores());
+  // Print welcome banner
+  if (!options.quiet && !options.cat && !options.toPly) {
+    if (sizeof(void *) == 4)
+      printf(
+          "*** WARNING: This is a 32-bit build of pbrt. It will crash "
+          "if used to render highly complex scenes. ***\n");
+    printf("pbrt version 3 (built %s at %s) [Detected %d cores]\n", __DATE__, __TIME__, NumSystemCores());
 #ifndef NDEBUG
-        LOG(INFO) << "Running debug build";
-        printf("*** DEBUG BUILD ***\n");
-#endif // !NDEBUG
-        printf(
-            "Copyright (c)1998-2018 Matt Pharr, Greg Humphreys, and Wenzel "
-            "Jakob.\n");
-        printf(
-            "The source code to pbrt (but *not* the book contents) is covered "
-            "by the BSD License.\n");
-        printf("See the file LICENSE.txt for the conditions of the license.\n");
-        fflush(stdout);
-    }
-    pbrtInit(options);
-    // Process scene description
-    if (filenames.empty()) {
-        // Parse scene from standard input
-        pbrtParseFile("-");
-    } else {
-        // Parse scene from input files
-        for (const std::string &f : filenames)
-            pbrtParseFile(f);
-    }
-    pbrtCleanup();
-    return 0;
+    LOG(INFO) << "Running debug build";
+    printf("*** DEBUG BUILD ***\n");
+#endif  // !NDEBUG
+    printf(
+        "Copyright (c)1998-2018 Matt Pharr, Greg Humphreys, and Wenzel "
+        "Jakob.\n");
+    printf(
+        "The source code to pbrt (but *not* the book contents) is covered "
+        "by the BSD License.\n");
+    printf("See the file LICENSE.txt for the conditions of the license.\n");
+    fflush(stdout);
+  }
+  pbrtInit(options);
+  // Process scene description
+  if (filenames.empty()) {
+    // Parse scene from standard input
+    pbrtParseFile("-");
+  } else {
+    // Parse scene from input files
+    for (const std::string &f : filenames) pbrtParseFile(f);
+  }
+  pbrtCleanup();
+  return 0;
 }
